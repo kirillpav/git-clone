8d3406aa5e760e1318986aaf4485d6a4d3c8d8cc
kirill pavlov <2020kirillp@gmail.com>
2024-07-03 11:53:57 +0300

    Branch functinality and other class refacturing

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 function Git(name) {
 	this.name = name;
 	this.lastCommitId = -1;
-	this.HEAD = null; // Ref to last commit
+
+	let master = new Branch("master", null);
+
+	this.HEAD = master; // Ref to master branch
 }
 
 // Commit Class
@@ -11,17 +14,24 @@ function Commit(id, parent, message) {
 	this.parent = parent;
 	this.message = this.message;
 }
+
+// Branch Class
+function Branch(name, commit) {
+	this.name = name;
+	this.commit = commit;
+}
 // Adding Commit to git class
 Git.prototype.commit = function (message) {
-	let commit = new Commit(++this.lastCommitId, this.HEAD, message);
-	this.HEAD = commit;
+	let commit = new Commit(++this.lastCommitId, this.HEAD.commit, message);
+	this.HEAD.commit = commit;
+
 	return commit;
 };
 
 // Log functionality on Git class
 Git.prototype.log = function () {
-	let commit = this.HEAD;
-	let history = [];
+	let commit = this.HEAD.commit,
+		history = [];
 
 	while (commit) {
 		history.push(commit);
