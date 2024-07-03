8530faff258302c42581051f9b110605893d690f
kirill pavlov <2020kirillp@gmail.com>
2024-07-03 11:39:11 +0300

    Finished and tested log functionality

--- main.js
+++ main.js
@@ -2,23 +2,43 @@
 function Git(name) {
 	this.name = name;
 	this.lastCommitId = -1;
+	this.HEAD = null; // Ref to last commit
 }
 
-let repo = new Git("my-repo");
-
 // Commit Class
-function Commit(id) {
+function Commit(id, parent, message) {
 	this.id = id;
+	this.parent = parent;
+	this.message = this.message;
 }
 // Adding Commit to git class
 Git.prototype.commit = function (message) {
-	let commit = new Commit(++this.lastCommitId, message);
+	let commit = new Commit(++this.lastCommitId, this.HEAD, message);
+	this.HEAD = commit;
 	return commit;
 };
 
 // Log functionality on Git class
 Git.prototype.log = function () {
+	let commit = this.HEAD;
 	let history = [];
+
+	while (commit) {
+		history.push(commit);
+		// Following the parent
+		commit = commit.parent;
+	}
 
 	return history;
 };
+
+// Commit history Tests
+console.log("Git.log() Tests");
+let repo = new Git("test-repo");
+repo.commit("Initial commit");
+repo.commit("Second commit");
+
+let log = repo.log();
+console.assert(log.length === 2); // Should have two commits
+console.assert(!!log[0] && log[0].id === 1);
+console.assert(!!log[1] && log[1].id === 0);
